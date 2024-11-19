a2a1e5fa5100534127bd9cecf2771dfd02951a1d
Jeevanand-Github <jeevanandmaurya@gmail.com>
2024-11-19 12:28:34 +0530

    adding in other lang

---
+++ tictactoe.java
@@ -0,0 +1,19 @@
+
+public class tictactoe {
+
+    public static void main(String[] args) {
+
+    int a,b,c,sum;
+
+    a=20;
+    b=226;
+    c=-36;
+
+    sum=a+b+c;
+        System.out.print("Sum of a , b and c: ");
+        System.out.println(sum);
+
+
+    }
+
+}
