cf6a6709585d33563ed578d2103fa51d1cf36e84
Jeevanand-Github <jeevanandmaurya@gmail.com>
2025-02-03 01:07:03 +0530

    update

--- tictactoe_new.c
+++ tictactoe_new.c
@@ -23,11 +23,33 @@ O WINS
 // Initialization of variables
 int inplay = 1; // 0-None 1-P1 2-P2 Player Currently InPlay
 int n = 3;
+char ch[3] = {'_', 'O', 'X'};
 int gamestate = 1;
 int state[3][3] = {0};
+int x = 0, y = 0;
+int pInput = 0;
 
-void printBoard(int *entries, char *playerChar, int entriCount, int charCount)
+void update()
 {
+
+    // Move Cursor
+    // Print InPlaying
+    printf("\033[14;14H%d", inplay);
+    // Take Input
+    printf("\033[6C");
+    scanf("%d", &pInput);
+    if (pInput > 0 && pInput < 10 && (!state[(pInput-1) / 3][(pInput-1) % 3]))
+    {
+        // Updating State
+        state[(pInput-1) / 3][(pInput-1) % 3] = inplay;
+        // Printing O/X
+        x = 16 + 2 * ((pInput-1) % 3);
+        y = 8 + (pInput-1) / 3;
+        printf("\033[%d;%dH%c", y, x, ch[inplay]);
+
+        // Switching InPlaying
+        inplay = 3 - inplay;
+    }
 }
 
 int checkWinner(int *entries, int entriCount)
@@ -45,7 +67,7 @@ int main()
     // int state[3][3] = {0};
 
     // Introduction and Instruction for the Game
-    printf("\n\n            TIC TAC TOE      \n\n");
+    printf("\n\n            \033[1m\033[48;5;21mTIC TAC TOE\033[0m      \n\n");
     printf("      Player 1->O   Player 2->X\n");
     // Seperation margin
     printf("\n\n");
@@ -81,7 +103,7 @@ int main()
     printf("\n\n");
 
     // Player Move
-    int pInput;
+
     for (int i = 0; i < 6; i++)
     {
         printf(" ");
@@ -91,30 +113,37 @@ int main()
     {
         printf(" ");
     }
-    printf("Player %d->\n\n", inplay);
+    printf("Player %d    ->\n\n", inplay);
 
     for (int i = 0; i < 6; i++)
     {
         printf(" ");
     }
+    printf("GameState   ->Ongoing \n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf(" ");
+    }
+    printf("Result      ->NA \n");
 
-    printf("GameState ->Ongoing \n");
     while (gamestate)
     {
-        // Player Input
-        scanf("%d", &pInput);
-        if (!state[pInput / 3][pInput % 3 - 1])
-        {
+        // // Player Input
+        // scanf("%d", &pInput);
+        // if (!state[pInput / 3][pInput % 3 - 1])
+        // {
 
-            // Updating State
-            state[pInput / 3][pInput % 3 - 1] = inplay;
-            // Switching InPlaying
-            inplay = 3 - inplay;
-            main();
-        }
-       
+        //     // Updating State
+        //     state[pInput / 3][pInput % 3 - 1] = inplay;
+        //     // Switching InPlaying
+        //     inplay = 3 - inplay;
+
+        //     main();
+        // }
+
+        update();
     }
-
+    printf("\007");
     // Bottom Margin
     for (int i = 0; i < 12; i++)
     {
