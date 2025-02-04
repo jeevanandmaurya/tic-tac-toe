a3b36d0773a9c850cbb8fa01bb16e71bb8d6a12a
Jeevanand-Github <jeevanandmaurya@gmail.com>
2025-02-04 14:54:42 +0530

    update

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("\033[1mBold Text\033[0m\n");       // Bold
+    printf("\033[9mUnderlined\033[0m\n");     // Underline
+    printf("\033[7mInverted\033[0m\n");       // Inverted colors
+    return 0;
+}
